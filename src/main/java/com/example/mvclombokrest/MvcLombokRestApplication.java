c4fb4689e18fea514bca5e40fb04cfb93ffd235f
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-07-17 21:43:08 -0300

    feat:initial repo

---
+++ src/main/java/com/example/mvclombokrest/MvcLombokRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvclombokrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLombokRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcLombokRestApplication.class, args);
+    }
+
+}
